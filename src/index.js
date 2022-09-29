302da8429aa37ed0358cd3b490bbb74c65d9929f
origami <1071242996@qq.com>
2022-09-29 15:35:19 +0800

    自己实现一个useState

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const rootElement = document.getElementById("root");
+
+let _state;
+
+function myUseState(initialValue) {
+  _state = _state === undefined ? initialValue : _state;
+  function setState(newState) {
+    _state = newState;
+    render();
+  }
+  return [_state, setState];
+}
+
+const render = () => ReactDOM.render(<App />, rootElement);
+
+function App() {
+  const [n, setN] = myUseState(0);
+  return (
+    <div className="App">
+      <p>{n}</p>
+      <p>
+        <button onClick={() => setN(n + 1)}>+1</button>
+      </p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, rootElement);
