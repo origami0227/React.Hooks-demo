ca95c0e6c63f79e533d3bf2e594d7495cfb8c7e4
origami <1071242996@qq.com>
2022-09-30 10:35:32 +0800

    setState接受函数

--- src/index.js
+++ src/index.js
@@ -2,18 +2,16 @@ import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
 function App() {
-    const [user,setUser] = useState({name:'Origami', age: 18})
+    const [n, setN] = useState(0)
     const onClick = ()=>{
-        setUser({
-            ...user,//需要将原来的对象拷贝一遍在进行覆盖
-            name: 'Jack'
-        })
+        setN(i=>i+1)
+        setN(i=>i+1)
     }
     return (
         <div className="App">
-            <h1>{user.name}</h1>
-            <h2>{user.age}</h2>
-            <button onClick={onClick}>Click</button>
+            <h1>n: {n}</h1>
+
+            <button onClick={onClick}>+2</button>
         </div>
     );
 }
