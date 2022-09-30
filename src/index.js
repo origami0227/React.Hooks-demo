eeb45672cf247913d51a88c056a656cd55fb7178
origami <1071242996@qq.com>
2022-09-30 14:43:45 +0800

    useReducer改写

--- src/index.js
+++ src/index.js
@@ -1,77 +1,29 @@
-import React, { useReducer } from "react";
+import React, {useState,useReducer} from "react";
 import ReactDOM from "react-dom";
 
-const initFormData = {
-    name: "",
-    age: 18,
-    nationality: "汉族"
-};
-
-function reducer(state, action) {
-    switch (action.type) {
-        case "patch":
-            return { ...state, ...action.formData };
-        case "reset":
-            return initFormData;
-        default:
-            throw new Error();
+const initial = {
+    n:0
+}//第一步创建初始值
+const reducer = (state,action)=>{
+    if(action.type === 'add'){
+        return {n:state.n+1}
+    }else if(action.type ==='multi'){
+        return {n:state.n*2}
+    }else{
+        throw new Error('unknown type')
     }
-}
-
+}//第二步创建所有操作
 function App() {
-    const [formData, dispatch] = useReducer(reducer, initFormData);
-    // const patch = (key, value)=>{
-    //   dispatch({ type: "patch", formData: { [key]: value } })
-    // }
-    const onSubmit = () => {};
-    const onReset = () => {
-        dispatch({ type: "reset" });
-    };
+    const [state,dispatch] = useReducer(reducer,initial)//第三步传给useReducer获得读写API
+    const onClick = ()=>{
+        dispatch({type:'add'})//第四步调用，选择操作类型
+    }
     return (
-        <form onSubmit={onSubmit} onReset={onReset}>
-            <div>
-                <label>
-                    姓名
-                    <input
-                        value={formData.name}
-                        onChange={e =>
-                            dispatch({ type: "patch", formData: { name: e.target.value } })
-                        }
-                    />
-                </label>
-            </div>
-            <div>
-                <label>
-                    年龄
-                    <input
-                        value={formData.age}
-                        onChange={e =>
-                            dispatch({ type: "patch", formData: { age: e.target.value } })
-                        }
-                    />
-                </label>
-            </div>
-            <div>
-                <label>
-                    民族
-                    <input
-                        value={formData.nationality}
-                        onChange={e =>
-                            dispatch({
-                                type: "patch",
-                                formData: { nationality: e.target.value }
-                            })
-                        }
-                    />
-                </label>
-            </div>
-            <div>
-                <button type="submit">提交</button>
-                <button type="reset">重置</button>
-            </div>
-            <hr />
-            {JSON.stringify(formData)}
-        </form>
+        <div className="App">
+            <h1>n: {state.n}</h1>
+
+            <button onClick={onClick}>+2</button>
+        </div>
     );
 }
 
