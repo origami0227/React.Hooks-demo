ef3615a64444942c3a74250e5e32f91901c4ed78
origami <1071242996@qq.com>
2022-10-02 10:56:41 +0800

    forwardRef-demo

--- src/index.js
+++ src/index.js
@@ -1,42 +1,20 @@
-import React, { useMemo } from "react";
+import React, { useRef } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
 function App() {
-    const [n, setN] = React.useState(0);
-    const [m, setM] = React.useState(0);
-    const onClick = () => {
-        setN(n + 1);
-    };
-    const onClick2 = () => {
-        setM(m + 1);
-    };
-    const onClickChild = useMemo(() => {
-        const fn = div => {
-            console.log("on click child, m: " + m);
-            console.log(div);
-        };
-        return fn;
-    }, [m]); // 这里呃 [m] 改成 [n] 就会打印出旧的 m
+    const buttonRef = useRef(null);
     return (
         <div className="App">
-            <div>
-                <button onClick={onClick}>update n {n}</button>
-                <button onClick={onClick2}>update m {m}</button>
-            </div>
-            <Child2 data={m} onClick={onClickChild} />
+            <Button3 ref={buttonRef}>按钮</Button3>
         </div>
     );
 }
 
-function Child(props) {
-    console.log("child 执行了");
-    console.log("假设这里有大量代码");
-    return <div onClick={e => props.onClick(e.target)}>child: {props.data}</div>;
-}
-
-const Child2 = React.memo(Child);
+const Button3 = React.forwardRef((props, ref) => {
+    return <button className="red" ref={ref} {...props} />;
+});
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
